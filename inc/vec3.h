dafa334196e897785990bb5c781e8f06dca8efc7
Jaejun Shin <jaeshinkorean@gmail.com>
2023-11-02 22:44:20 +1030

    vectors wip

---
+++ inc/vec3.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec3.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeshin <jaeshin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 22:20:09 by jaeshin           #+#    #+#             */
+/*   Updated: 2023/11/02 22:35:48 by jaeshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VEC3_H
+#define VEC3_H
+
+#include "../libft/includes/libft.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct s_vec3
+{
+	double	x;
+	double	y;
+	double	z;
+}	t_vec3;
+
+
+
+
+#endif
